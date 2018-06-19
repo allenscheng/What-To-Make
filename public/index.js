c9d560f32d2cde7f54c01e02f4a56e623734f253
Allen Cheng <allencheng219@gmail.com>
2018-06-19 14:11:53 -0500

    created a search component page

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+/* global Vue, VueRouter, axios */
+
 var HomePage = {
   template: "#home-page",
   data: function() {
@@ -9,11 +11,26 @@ var HomePage = {
   methods: {},
   computed: {}
 };
+var Search = {
+  template: "#search",
+  data: function() {
+    return {
+      message: "Hello"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
-  scrollBehavior: function(to, from, savedPosition) {
-    return { x: 0, y: 0 };
-  }
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/search", component: Search }
+  ]
+
+  // scrollBehavior: function(to, from, savedPosition) {
+  //   return { x: 0, y: 0 };
+  // }
 });
 
 var app = new Vue({
